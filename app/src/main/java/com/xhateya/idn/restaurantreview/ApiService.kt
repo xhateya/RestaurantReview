00efbfc0af1ab737f2ae473ffa14d018bf800c3d
nabilaateya <natheya36@gmail.com>
2022-03-22 14:19:27 +0700

    Initial commit

---
+++ app/src/main/java/com/xhateya/idn/restaurantreview/ApiService.kt
@@ -0,0 +1,22 @@
+package com.xhateya.idn.restaurantreview
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+
+    fun getRestaurant(
+        @Path("id") id: String
+    ) : Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Athorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id")id:String,
+        @Field("name")name: String,
+        @Field("review")review: String,
+
+    ): Call<PostReviewResponse>
+}
